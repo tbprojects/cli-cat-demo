7afc71573ca2cdd58a8e406059b63e1b42f45a1a
tbprojects <info.tbprojects@gmail.com>
2018-03-20 19:59:59 +0100

    added specs for CatListComponent and CatComponent

--- src/app/cats/cat-list/cat-list.component.spec.ts
+++ src/app/cats/cat-list/cat-list.component.spec.ts
@@ -1,6 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 import { CatListComponent } from './cat-list.component';
+import { Cat } from '../cat';
+import { CatService } from '../cat.service';
+
+@Component({
+  selector: 'app-cat',
+  template: 'mocked cat'
+})
+export class CatMockComponent {
+  @Input() cat: Cat;
+  @Input() index: number;
+  @Output() catRemoved = new EventEmitter();
+}
+
+export class CatMockService {
+  getCats(): Observable<Cat[]> {
+    return of([{id: 1, src: ''}, {id: 1, src: ''}]);
+  }
+
+  addCat(cat: Cat): Observable<Cat> {
+    return of({id: 1, src: ''});
+  }
+
+  removeCat(id: number): Observable<Cat> {
+    return of({id: 1, src: ''});
+  }
+}
 
 describe('CatListComponent', () => {
   let component: CatListComponent;
@@ -8,18 +38,67 @@ describe('CatListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CatListComponent ]
+      declarations: [ CatListComponent, CatMockComponent],
+      providers: [{provide: CatService, useClass: CatMockService}],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(CatListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render 2 cats', () => {
+    const catNodes = fixture.debugElement.queryAll(By.css('app-cat'));
+    expect(catNodes.length).toEqual(2);
+  });
+
+  it('should render form headline', () => {
+    const formHeadline = fixture.debugElement.query(By.css('h2'));
+    expect(formHeadline.nativeElement.textContent).toEqual('Add your kitty');
+  });
+
+  describe('when cat is added', () => {
+    let catService;
+
+    beforeEach(() => {
+      catService = TestBed.get(CatService);
+      spyOn(catService, 'addCat').and.callThrough();
+      const button = fixture.debugElement.query(By.css('form button'));
+      const input = fixture.debugElement.query(By.css('input[name="src"]'));
+      input.nativeElement.value = 'http://localhost/kitten.png';
+      input.nativeElement.dispatchEvent(new Event('input'));
+      button.nativeElement.click();
+      fixture.detectChanges();
+    });
+
+    it('should render 3 cats', () => {
+      const catNodes = fixture.debugElement.queryAll(By.css('app-cat'));
+      expect(catNodes.length).toEqual(3);
+    });
+
+    it('should call addCat on CatService', () => {
+      expect(catService.addCat).toHaveBeenCalledWith({src: 'http://localhost/kitten.png'});
+    });
+  });
+
+  describe('when cat is removed', () => {
+    beforeEach(() => {
+      const catComponent = fixture.debugElement.query(By.css('app-cat'));
+      catComponent.triggerEventHandler('catRemoved', catComponent.componentInstance.cat);
+      fixture.detectChanges();
+    });
+
+    it('should render 1 cat', () => {
+      const catNodes = fixture.debugElement.queryAll(By.css('app-cat'));
+      expect(catNodes.length).toEqual(1);
+    });
   });
 });
--- src/app/cats/cat/cat.component.spec.ts
+++ src/app/cats/cat/cat.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { CatComponent } from './cat.component';
 
 describe('CatComponent', () => {
@@ -16,10 +16,30 @@ describe('CatComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CatComponent);
     component = fixture.componentInstance;
+    component.cat = {id: 1, src: '/'};
+    component.index = 1;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render cat index', () => {
+    const paragraph = fixture.debugElement.query(By.css('p'));
+    expect(paragraph.nativeElement.textContent).toContain('Cat #1');
+  });
+
+  describe('when remove button is clicked', () => {
+    beforeEach(() => {
+      spyOn(component.catRemoved, 'emit');
+      const button = fixture.debugElement.query(By.css('button'));
+      button.triggerEventHandler('click', {});
+    });
+
+    it('should emit catRemoved event', () => {
+      expect(component.catRemoved.emit).toHaveBeenCalledWith(component.cat);
+    });
+  });
 });
+
