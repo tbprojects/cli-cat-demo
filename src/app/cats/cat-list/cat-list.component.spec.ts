f715734319285e6dae3377afa1f15a67b8925484
tbprojects <info.tbprojects@gmail.com>
2018-04-25 11:30:39 +0200

    add page object to CatListComponent specs

--- src/app/cats/cat-list/cat-list.component.spec.ts
+++ src/app/cats/cat-list/cat-list.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, DebugElement, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { Observable } from 'rxjs/Observable';
@@ -31,6 +31,36 @@ export class CatMockService {
 
   removeCat(id: number): Observable<Cat> {
     return of({id: 1, src: 'kitty-1.jpg', name: 'kitty-1'});
+  }
+}
+
+class Page {
+  constructor(private fixture: ComponentFixture<CatListComponent>) {}
+
+  fillInput(input: DebugElement, value: string) {
+    input.nativeElement.value = value;
+    input.nativeElement.dispatchEvent(new Event('input'));
+  }
+
+  submitForm() {
+    this.submitButton.nativeElement.click();
+    this.fixture.detectChanges();
+  }
+
+  get srcInput(): DebugElement {
+    return this.fixture.debugElement.query(By.css('input[name="src"]'));
+  }
+
+  get nameInput(): DebugElement {
+    return this.fixture.debugElement.query(By.css('input[name="name"]'));
+  }
+
+  get submitButton(): DebugElement {
+    return this.fixture.debugElement.query(By.css('form button'));
+  }
+
+  get catNodes(): DebugElement[] {
+    return this.fixture.debugElement.queryAll(By.css('app-cat'));
   }
 }
 
@@ -70,33 +100,25 @@ describe('CatListComponent', () => {
 
   describe('when cat is added', () => {
     let catService;
+    let page: Page;
 
     beforeEach(() => {
       catService = TestBed.get(CatService);
       spyOn(catService, 'addCat').and.callThrough();
 
-      const srcInput = fixture.debugElement.query(By.css('input[name="src"]'));
-      srcInput.nativeElement.value = 'http://localhost/kitten.png';
-      srcInput.nativeElement.dispatchEvent(new Event('input'));
-
-      const nameInput = fixture.debugElement.query(By.css('input[name="name"]'));
-      nameInput.nativeElement.value = 'mruczek';
-      nameInput.nativeElement.dispatchEvent(new Event('input'));
-
-      const button = fixture.debugElement.query(By.css('form button'));
-      button.nativeElement.click();
-
-      fixture.detectChanges();
+      page = new Page(fixture);
+      page.fillInput(page.srcInput, 'http://localhost/kitten.png');
+      page.fillInput(page.nameInput, 'mruczek');
+      page.submitForm();
     });
 
     it('should render 3 cats', () => {
-      const catNodes = fixture.debugElement.queryAll(By.css('app-cat'));
-      expect(catNodes.length).toEqual(3);
+      expect(page.catNodes.length).toEqual(3);
     });
 
     it('should call addCat on CatService', () => {
-      expect(catService.addCat)
-        .toHaveBeenCalledWith({src: 'http://localhost/kitten.png', name: 'mruczek'});
+      const expectedCat = {src: 'http://localhost/kitten.png', name: 'mruczek'}
+      expect(catService.addCat).toHaveBeenCalledWith(expectedCat);
     });
   });
 
